f0b13ac5c93b098d53f4d4811919989c2d39ba5d
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-05-04 11:09:15 +0530

    order-schema

--- projbackend/models/order.js
+++ projbackend/models/order.js
@@ -1,8 +1,21 @@
 const monggose = require('mongoose');
 const {ObjectId} = mongoose.Schema;
 
-const OrderSchema = new mongoose.Schema({
 
+const ProductCartSchema = new mongoose.Schema({
+    product: {
+        type: ObjectId,
+        ref: "Product"
+    },
+    name: String,
+    count: String,
+    price: String
+});
+
+const ProductCart = mongoose.model("ProductCart",ProductCartSchema);
+
+
+const OrderSchema = new mongoose.Schema({
     products: [ProductCartSchema],
     transaction_id: {},
     amount: {type: Number},
@@ -14,3 +27,7 @@ const OrderSchema = new mongoose.Schema({
     }
 }, {timestamps: true}
 );
+
+const order = mongoose.model("Order",OrderSchema);
+
+module.exports = {Order,ProductCart};
