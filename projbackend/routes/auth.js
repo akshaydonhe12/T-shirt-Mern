877389d6f55541f4348c681e794066d9dac9015c
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-05-10 10:48:42 +0530

    signup-validation

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 const {signout,signup} = require('../controllers/auth');
 
 
-router.post("/signup", signup);
+router.post("/signup",[
+    check("name", "Name should be at least 2 char").isLength({ min: 2 }),
+    check("email", "Email is required").isEmail(),
+    check("password", "Password should be at least 2 char").isLength({ min: 3 }),
+], signup);
 router.get("/signout", signout);
 
 module.exports = router;
