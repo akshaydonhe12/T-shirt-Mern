20b42ba1700998108ab72da02e7edacc2ba813e7
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-05-16 11:36:19 +0530

    User-Update

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -19,8 +19,25 @@ exports.getUser =(req, res,) =>{
 
     req.profile.salt = undefined;
     req.profile.encry_password = undefined;
-    req.profile.createdAt = undefined;
-    req.profile.updatedAt = undefined;
-    
     return res.json(req.profile);
 };
+
+exports.updateUser =(req, res) =>{
+    User.findByIdAndUpdate(
+       {_id: req.profile._id},
+       {$set: req.body},
+       {new : true, useFindAndModify: false },
+       (err,user)=>{
+           if(err){
+               return res.status(400).json({
+                  error: "You are not authorized to update this user"
+               });
+           }
+
+           user.salt = undefined;
+           user.encry_password = undefined;
+           res.json(user); 
+       }
+
+    );
+}; 
--- projbackend/routes/user.js
+++ projbackend/routes/user.js
@@ -2,12 +2,12 @@ const express =require ("express");
 const router =express.Router(); 
 
 
-const{getUserById, getUser} = require("../controllers/user");
+const{getUserById, getUser, updateUser} = require("../controllers/user");
 const{isSigndIn, isAuthenticated, isAdmin} = require("../controllers/auth");
 
 
 router.param("userId", getUserById);
-router.get("/user/:userId",isSigndIn, isAuthenticated,getUser);
-
+router.get("/user/:userId",isSigndIn, isAuthenticated,  getUser);
+router.put("/user/:userId",isSigndIn, isAuthenticated, updateUser)
 
 module.exports = router;
