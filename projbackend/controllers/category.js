f92b6eaeab7ff1c3ee061b97b298247570a1209f
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-05-17 11:52:53 +0530

    category-controller

---
+++ projbackend/controllers/category.js
@@ -0,0 +1,16 @@
+const Category = require("../models/category");
+
+
+exports.getCategoryById = (req, res, next, id) =>{
+    
+    Category.findById(id).exec((err,cate)=>{
+        if(err){
+            return res.status(400).json({
+                error: "category Not Found In DB"
+            });
+        }
+        req.Category.cate;
+        next();
+    });
+
+};
