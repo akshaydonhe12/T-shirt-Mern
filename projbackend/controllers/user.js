b73ba2b7106fd4caceb053d4508c68d79b35c3d9
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-05-17 11:22:08 +0530

    orderpurchaselist

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -58,3 +58,35 @@ exports.userPurchaseList = (req, res) =>{
     });
 
 };
+
+
+exports.pushOrderInPurchaseList = (req, res) =>{
+    let purchases = [];
+    req.body.order.products.forEach(product =>{
+        purchases.push({
+            _id: product._id,
+            name: product.name,
+            description: product.description,
+            category: product.category,
+            quantity: product.quantity,
+            amount: req.body.order.amount,
+            transaction_id: req.body.order.transaction_id
+        });
+    });
+
+    //store in DB
+
+    User.findOneAndUpdate(
+        {_id: req.profile._id},
+        {$push: { purchases: purchases} },
+        {new:true},
+        (err,purchases)=>{
+            if(err){
+                return res.status(400).json({
+                    error:"Unable to save Purchase List"
+                });
+            }
+                next();
+            }
+    );
+};
