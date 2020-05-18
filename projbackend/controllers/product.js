a577b1ecc0c5a2bf175daeca61360a3af99004e1
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-05-18 18:21:06 +0530

    product-populate

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -2,6 +2,7 @@ const Product = require("../models/product");
 
 exports.getProductById = (req, res, next, id) => {
     Product.findById(id)
+    .populate("category")
     .exec((err, product) => {
         if(err){
             return res.status(400).json({
