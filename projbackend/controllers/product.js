ecbf665eb0afe6f0736579fbd1d685443d9f321f
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-05-21 17:25:51 +0530

    updateStock

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -166,3 +166,24 @@ exports.getAllProducts = (req,res) =>{
     });
 };
 
+exports.updateStock = (req, res, next) => {
+    
+    let myOperations = req.body.order.produtcs.map(prod =>{
+        return {
+            updateOne :{
+                filter: {_id: prod._id},
+                update: {$inc:{stock: -prod.count, sold: +prod.count}}
+
+            }
+        };
+    });
+
+    Product.bulkWrite(myOperations, {}, (err, products) =>{
+       if (err){
+            return res.status(400).json({
+            error: "Bulk Operation failed"
+        })
+    }
+       next();
+    });
+} ;
