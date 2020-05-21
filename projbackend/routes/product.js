9dbd7b49eaf3c9924d45729dd6136869da98538a
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-05-21 11:29:57 +0530

    photo-route

--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const {getProductById, createProduct, getProduct} = require("../controllers/product");
+const {getProductById, createProduct, getProduct, photo} = require("../controllers/product");
 const {isSigndIn, isAuthenticated, isAdmin} = require("../controllers/auth");
 const {getUserById} = require("../controllers/user");
 
@@ -15,5 +15,6 @@ router.param("productId", getProductById);
 router.post("/product/create/:userId", isSigndIn, isAuthenticated, isAdmin, createProduct);
 
 router.get("/product/:productId", getProduct)
+router.get("product/photo/:productId", photo)
 
 module.exports = router; 
