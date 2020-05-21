b936c46916b734d78f73753487fea5c2ac87d5f0
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-05-21 11:57:36 +0530

    productUpdate-route

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -105,5 +105,40 @@ exports.deleteProduct= (req, res) => {
 
 //update controller
 exports.updateProduct= (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
 
-}
+    form.parse(req, (err, fields, file) =>{
+        if(err){
+            return res.status(400).json({
+                error:"Problem with image"
+            });
+        }
+
+    let product = req.product;
+    product=_.extend
+
+//handle file here
+
+    if (file.photo){
+        if (file.photo.size > 3000000) {
+            return res.status(400).json({
+                error: "File size to big"
+            });
+        }
+        product.photo.data = fs.readFileSync(file.photo.path);
+        product.photo.contentType = file.photo.type;
+    }
+
+ //save in  DB
+ product.save((err, product) => {
+    if (err) {
+      res.status(400).json({
+        error: "Saving tshirt in DB failed"
+      });
+    }
+    res.json(product);
+  });
+});
+};
+
