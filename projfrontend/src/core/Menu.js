f1b41fcf11229a76667952c38a20685c8fffd231
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-05-27 18:33:05 +0530

    Adding-Style_toNavbar

--- projfrontend/src/core/Menu.js
+++ projfrontend/src/core/Menu.js
@@ -1,42 +1,51 @@
 import React from "react";
 import {Link, withRouter} from "react-router-dom";
 
+const currentTab = (history, path) => {
+    if(history.location.pathname ===path) {
+        return {color: "#ffffff"}
+    } else {
+        return {color: "#ff5200"}
+    }
+        
+}
 
-const Menu = () => (
+
+const Menu = ({history}) => (
     <div>
         <ui className="nav nav-tabs bg-dark">
             <li className="nav-item">
-                <Link className="nav-link" to="/">
+                <Link style={currentTab(history, "/")} className="nav-link" to="/">
                     Home
                 </Link>
             </li>
             <li className="nav-item">
-                <Link className="nav-link" to="/">
+                <Link style={currentTab(history, "cart")} className="nav-link" to="cart">
                     Cart
                 </Link>
             </li>
             <li className="nav-item">
-                <Link className="nav-link" to="/">
+                <Link style={currentTab(history, "/user/dashboard")} className="nav-link" to="/user/dasboard">
                     Dashboard
                 </Link>
             </li>
             <li className="nav-item">
-                <Link className="nav-link" to="/">
+                <Link style={currentTab(history, "/A.dashboard")} className="nav-link" to="/A.dashboard">
                     A.Dashboard
                 </Link>
             </li>
             <li className="nav-item">
-                <Link className="nav-link" to="/">
+                <Link style={currentTab(history, "/Signup")} className="nav-link" to="/Signnup">
                     Signup
                 </Link>
             </li>
             <li className="nav-item">
-                <Link className="nav-link" to="/">
+                <Link style={currentTab(history, "/Signin")} className="nav-link" to="/Signin">
                     Signin
                 </Link>
             </li>
             <li className="nav-item">
-                <Link className="nav-link" to="/">
+                <Link style={currentTab(history, "/Signout")} className="nav-link" to="/Signout">
                     Signout
                 </Link>
             </li>
