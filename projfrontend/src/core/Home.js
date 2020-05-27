9c70123bd969719b3b2c725f4f2d341b669ad268
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-05-27 18:19:07 +0530

    injected_Navbar

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -7,7 +7,17 @@ export default function Home() {
   console.log("API IS", API);
   return (
     <Base title="Home Page">
-      <h1 className="text-white">Hello front end</h1>
+      <div className="row">
+      <div className="col-4">
+        <button className="btn btn-success">TEST</button>
+      </div>
+      <div className="col-4">
+        <button className="btn btn-success">TEST</button>
+      </div>
+      <div className="col-4">
+        <button className="btn btn-success">TEST</button>
+      </div>
+      </div>
     </Base>
   );
 }
