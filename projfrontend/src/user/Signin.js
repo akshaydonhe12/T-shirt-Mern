548c74149e98271ffe7ce33d184ac0dd423336df
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-05-27 19:01:35 +0530

    Create-formFor-Signin-Signup

--- projfrontend/src/user/Signin.js
+++ projfrontend/src/user/Signin.js
@@ -4,9 +4,33 @@ import {Link} from "react-router-dom";
 
 
 const Signin = () => {
+
+     const signInForm = () => {
+        return (
+            <div className="row">
+                <div className="col-6 offset-sm-3 text-left">
+                    <form> 
+                        <div className="form-group">
+                            <label className="text-light">Email</label>
+                            <input className="form-control" type="email">  
+                            </input>
+                        </div>
+                        <div className="form-group">
+                            <label className="text-light">Password</label>
+                            <input className="form-control" type="password">  
+                            </input>
+                        </div>
+                        <button className="btn btn-success btn-block">Submit</button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+
+
     return (
         <Base title="Sign in page" description="A page for user to signin!">
-            <h1>Sign in works</h1>
+            {signInForm()}
         </Base>
     );
 };
--- projfrontend/src/user/Signup.js
+++ projfrontend/src/user/Signup.js
@@ -4,12 +4,42 @@ import {Link} from "react-router-dom";
 
 
 const Signup = () => {
+
+    const sinUpForm = () => {
+        return (
+            <div className="row">
+                <div className="col-6 offset-sm-3 text-left">
+                    <form>
+                        <div className="form-group">
+                            <label className="text-light">Name</label>
+                            <input className="form-control" type="text">  
+                            </input>
+                        </div>
+                        <div className="form-group">
+                            <label className="text-light">Email</label>
+                            <input className="form-control" type="email">  
+                            </input>
+                        </div>
+                        <div className="form-group">
+                            <label className="text-light">Password</label>
+                            <input className="form-control" type="password">  
+                            </input>
+                        </div>
+                        <button className="btn btn-success btn-block">Submit</button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <Base title="Sign up page" description="A page for user to signup!">
-            <h1>Sign up works</h1>
+            {sinUpForm()}
         </Base>
     );
 };
+
+
 
 
 export default Signup;
