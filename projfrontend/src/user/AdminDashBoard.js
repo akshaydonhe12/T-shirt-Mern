d9d2285b9644b701f55f533e965a550e305784e4
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-05-30 18:00:39 +0530

    create-Admi_Navigation

--- projfrontend/src/user/AdminDashBoard.js
+++ projfrontend/src/user/AdminDashBoard.js
@@ -1,13 +1,61 @@
 
 import React from "react";
 import Base from '../core/Base';
-
+import {isAutheticated} from "../auth/helper/index";
+import { Link } from "react-router-dom";
 
 
 const AdminDashBoard=() => {
+
+  const {
+    user: {name, email, role}
+} = isAutheticated ();
+
+
+const adminLeftSide = () => {
   return (
-    <Base title="AdminDashBoard page">
-      <h1> This is AdminDashBoard Page</h1>
+    <div className="card">
+      <h4 className="card-header bg-dark text-white">Admin Navigation</h4>
+      <ul className="list-group">
+        <li className="list-group-item">
+          <Link to="/admin/create/category" className="nav-link text-success">Create Categories</Link>
+        </li>
+        <li className="list-group-item">
+          <Link to="/admin/create/Product" className="nav-link text-success">Create Products</Link>
+        </li>
+        <li className="list-group-item">
+          <Link to="/admin/products" className="nav-link text-success">Manage Products</Link>
+        </li>
+        <li className="list-group-item">
+          <Link to="/admin/orders" className="nav-link text-success">Manage Orders</Link>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+
+const adminRightSide = () => {
+  return (
+    <div>
+      Hi
+    </div>
+  )
+}
+
+
+  return (
+    <Base title="Welcome to admin area"
+     description="manage all of your products here"
+     className="container bg-success p-4"
+     
+     >
+      <div className="row">
+        <div className="col-3">{adminLeftSide()}</div>
+        <div className="col-9">{adminRightSide()} </div>
+
+      </div>
+
     </Base>
   );
 }
