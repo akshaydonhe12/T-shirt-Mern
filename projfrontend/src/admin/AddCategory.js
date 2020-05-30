c0cfd5ac2e26a4ee84d3ae9791a64956743b0e89
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-05-30 18:29:33 +0530

    Add-Category-route

---
+++ projfrontend/src/admin/AddCategory.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Base from "../core/Base";
+
+const AddCategory = () => {
+  return (
+   <Base title="Create a category here" 
+   description="Add a new category for new t-shirt"
+   className="container bg-success p-4"
+   >
+       <div className="row bg-white rounded">
+           <div className="col-md-8 offset-md-2">
+               Hi,Welcome To T-shirt Shop.
+           </div>
+       </div>
+   </Base>
+  );
+}
+
+
+
+export default AddCategory;
