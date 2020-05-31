a115d39350ba4b59d358ed3e7323cbfba1b2c788
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-05-31 11:07:33 +0530

    fix-bug-adminapicall

--- projfrontend/src/admin/AddCategory.js
+++ projfrontend/src/admin/AddCategory.js
@@ -2,11 +2,12 @@ import React, {useState} from "react";
 import Base from "../core/Base";
 import {isAutheticated} from "../auth/helper";
 import { Link } from "react-router-dom";
+import {createCategory} from "./helper/adminapicall";
 
 const AddCategory = () => {
 
-    const [ name, setName] = useState("initialState");
-    const [ error, setErrot] = useState(false);
+    const [ name, setName] = useState("");
+    const [ error, setError] = useState(false);
     const [success, setSuccess] = useState(false);
 
     const {user, token} = isAutheticated();
@@ -19,17 +20,42 @@ const AddCategory = () => {
         )
     };
 
+    const handleChange = (event) =>{
+        setError("");
+        setName(event.target.value)
+    };
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        setError("");
+        setSuccess(false)
+
+        //backend request fired
+        createCategory(user._id, token, {name})
+        .then( data => {
+            if(data ?. error) {
+                setError(true)
+            } else {
+                setError("")
+                setSuccess(true)
+            }
+        });
+        
+    };
+
     const myCategoryForm = () => (
         <form>
             <div className="form-group">
                 <p className="lead">Enter the category</p>
                 <input type="text"
                  className="form-control my-3"
+                 onChange={handleChange}
+                 value={name}
                  autoFocus
                  required
                  placeholder="For Ex.Summer"
                  />
-                <button className="btn btn-outline-info">Create Category</button>
+                <button onClick={onSubmit} className="btn btn-outline-info">Create Category</button>
             </div>
         </form>
     );
