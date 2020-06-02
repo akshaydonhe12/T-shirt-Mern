f3c4f731477299ca795696977a251778182b1660
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-06-02 18:03:35 +0530

    Update-products

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -11,6 +11,8 @@ import AddCategory from "./admin/AddCategory";
 import ManageCategories from "./admin/ManageCategories";
 import AddProduct from "./admin/AddProduct";
 import ManageProducts from "./admin/ManageProducts";
+import UpdateProduct from "./admin/UpdateProduct";
+
 
 
 
@@ -29,6 +31,8 @@ const Routes = () => {
         <AdminRoute path="/admin/categories"  exact component={ManageCategories} />
         <AdminRoute path="/admin/create/Product"  exact component={AddProduct} />
         <AdminRoute path="/admin/products"  exact component={ManageProducts} />
+        <AdminRoute path="/admin/product/update/:productId"  exact component={UpdateProduct} />
+
 
 
 
