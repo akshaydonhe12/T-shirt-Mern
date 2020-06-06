e327b12982a51884d87fae804106dabfb6912fe5
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-06-06 11:03:07 +0530

    PaymentHelper-create

--- projfrontend/src/core/Cart.js
+++ projfrontend/src/core/Cart.js
@@ -1,55 +1,55 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "../styles.css";
 import { API } from "../backend";
 import Base from "./Base";
 import Card from "./Card";
-import {loadCart} from "./helper/cartHelper";
+import { loadCart } from "./helper/cartHelper";
 
-const Cart = ()=> {
- 
-    const [products, setProducts] = useState([]);
-    const [reload, setReload] = useState(false)
+const Cart = () => {
+  const [products, setProducts] = useState([]);
+  const [reload, setReload] = useState(false);
 
-    useEffect(()=> {
-        setProducts(loadCart());
-    }, [reload]);
+  useEffect(() => {
+    setProducts(loadCart());
+  }, [reload]);
 
-    const loadAllProducts = () => {
-        return (
-            <div>
-                <h2>This Section is to load products.</h2>
-                {products.map((product, index) => (
-                    <Card 
-                    key ={index}
-                    product={product}
-                    removeFromCart={true}
-                    addtoCart={false}
-                    setReload={setReload}
-                    reload={reload}
-                    />)
-                )}
-            </div>
-        );
-    };
-
-    const loadCheckout = () => {
-        return (
-            <div>
-                <h2>This Section for Checkout.</h2>
-            </div>
-        );
-    };
-    
-   
+  const loadAllProducts = products => {
+    return (
+      <div>
+        <h2>This section is to load products</h2>
+        {products.map((product, index) => (
+          <Card
+            key={index}
+            product={product}
+            removeFromCart={true}
+            addtoCart={false}
+            setReload={setReload}
+            reload={reload}
+          />
+        ))}
+      </div>
+    );
+  };
+  const loadCheckout = () => {
+    return (
+      <div>
+        <h2>This section for checkout</h2>
+      </div>
+    );
+  };
 
   return (
-    <Base title="Cart Page" description="Ready To Checkout">
-      <div className="row text-center ">
-       <div className="col-6">{loadAllProducts()}</div>
-       <div className="col-6">{loadCheckout()}</div>
-
-          
+    <Base title="Cart Page" description="Ready to checkout">
+      <div className="row text-center">
+        <div className="col-6">
+          {products.length > 0 ? (
+            loadAllProducts(products)
+          ) : (
+            <h3>NO products </h3>
+          )}
         </div>
+        <div className="col-6">let's got for braintree</div>
+      </div>
     </Base>
   );
 };
